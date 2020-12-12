d7f565b8fdde9bec669fd5adc0360500aace7700
lemons305 <lemons.l3m0n5@gmail.com>
2020-12-13 04:23:42 +0700

    rm

--- blog/assets/js/blog.js
+++ blog/assets/js/blog.js
@@ -1,7 +1,7 @@
 function getPost(nem){
 	$.ajax({
 		type: "GET",
-		url: "https://lemons305.github.io/blog/post/"+nem,
+		url: "https://lemons305.github.io/blog/post/"+nem+".html",
 		success: function (res) {
 			$('#mainContent').html(res);
 		}
